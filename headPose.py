cc9ed53be040454609b9bb4ed71695974adb3398
Pratim-72 <72607121+Pratim-72@users.noreply.github.com>
2020-10-19 23:52:48 +0530

    Update headPose.py

--- headPose.py
+++ headPose.py
@@ -75,5 +75,5 @@ print("hacktober fest 2020")
 
 print("hacktober fest dit 2020");
 
-print("sukhmeet singh duggal ACM project fest 2020);
-
+print("sukhmeet singh duggal ACM project fest 2020");
+print("Pratim Maity ACM project fest 2020");
